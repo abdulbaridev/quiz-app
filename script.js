4b2e586b8f07875ee232bcf4a0b0e2ec6d61b3ce
Abdul Bari <125637321+abdulbari22385@users.noreply.github.com>
2024-10-31 14:31:38 +0500

    3 more questions has been added

--- script.js
+++ script.js
@@ -29,6 +29,26 @@ document.addEventListener("DOMContentLoaded", () => {
       ],
       answer: "William Shakespeare",
     },
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      choices: ["Mars", "Venus", "Jupiter", "Saturn"],
+      answer: "Mars",
+    },
+    {
+      question: "Who wrote 'Hamlet'?",
+      choices: [
+        "Charles Dickens",
+        "Jane Austen",
+        "William Shakespeare",
+        "Mark Twain",
+      ],
+      answer: "William Shakespeare",
+    },
   ];
 
   let currentQuestionIndex = 0;
